f962b46cdddbb308e302de17f791d1c0afe47d3a
Peter A. Higgins <pahiggins@cenabo.com>
2018-12-04 14:08:23 +0000

    WIP Day 2 - Part 2

--- README.md
+++ README.md
@@ -7,3 +7,7 @@ Part 2 - WIP
 ## Day 2
 Part 1 - Solved
 Part 2 - WIP
+
+## Day 3
+Part 1 - ?
+Part 2 - ?
--- day2/day2.js
+++ day2/day2.js
@@ -1,40 +1,80 @@
 const fs = require('fs');
 
+Array.prototype.diff = function(arr2) {
+  return this.filter(x => arr2.includes(x));
+}
+
+// const x = 'ubkfmdjxyzlbgkrotcepvswaqx';
+// const y = 'uikfmdkuyzlbgerotcepvswaqh';
+
+// const commonChars = [...x].diff([...y]);
+
+// console.log(x.length);
+// console.log(y.length);
+// console.log(commonChars.length);
+
+
 fs.readFile('./day2/input.txt', (err, input) => {
   if (err) throw err;
 
+  let compareIndex = 0;
+  const matches = [];
   const parsedInput = `${input}`.split('\n');
-  const sortedInput = parsedInput.map(string => [...string].sort().join());
-  const result = sortedInput.reduce((acc, string) => {
-    let twoCount = 0;
-    let threeCount = 0;
-    const occurancesTally = countOccurances(string);
 
-    for (let key in occurancesTally) {
-      twoCount = occurancesTally[key] === 2 ? twoCount + 1 : twoCount;
-      threeCount = occurancesTally[key] === 3 ? threeCount + 1 : threeCount;
+  parsedInput.forEach(string => {
+    let x = parsedInput[compareIndex];
+    let y = string;
+    console.log(x);
+    console.log(y);
+    const commonChars = [...x].diff([...y]);
+    // console.log(commonChars.length);
+
+    if (commonChars.length === 25) {
+      matches.push(x);
+      matches.push(y);
     }
 
-    acc[0] = twoCount > 0 ? acc[0] + 1 : acc[0];
-    acc[1] = threeCount > 0 ? acc[1] + 1 : acc[1];
+    compareIndex++;
+  });
 
-    return acc;
-  }, [0, 0]);
-
-  console.log(result);
+  // console.log(matches);
 });
 
-function countOccurances(string) {
-  const count = {};
-  const stringArr = [...string];
+// fs.readFile('./day2/input.txt', (err, input) => {
+//   if (err) throw err;
 
-  for (let i = 0; i < stringArr.length; i++) {
-    if (!count[stringArr[i]]) {
-      count[stringArr[i]] = 1;
-    } else {
-      count[stringArr[i]] = count[stringArr[i]] + 1;
-    }
-  }
+//   const parsedInput = `${input}`.split('\n');
+//   const sortedInput = parsedInput.map(string => [...string].sort().join());
+//   const result = sortedInput.reduce((acc, string) => {
+//     let twoCount = 0;
+//     let threeCount = 0;
+//     const occurancesTally = countOccurances(string);
 
-  return count;
-}
+//     for (let key in occurancesTally) {
+//       twoCount = occurancesTally[key] === 2 ? twoCount + 1 : twoCount;
+//       threeCount = occurancesTally[key] === 3 ? threeCount + 1 : threeCount;
+//     }
+
+//     acc[0] = twoCount > 0 ? acc[0] + 1 : acc[0];
+//     acc[1] = threeCount > 0 ? acc[1] + 1 : acc[1];
+
+//     return acc;
+//   }, [0, 0]);
+
+//   console.log(result);
+// });
+
+// function countOccurances(string) {
+//   const count = {};
+//   const stringArr = [...string];
+
+//   for (let i = 0; i < stringArr.length; i++) {
+//     if (!count[stringArr[i]]) {
+//       count[stringArr[i]] = 1;
+//     } else {
+//       count[stringArr[i]] = count[stringArr[i]] + 1;
+//     }
+//   }
+
+//   return count;
+// }
